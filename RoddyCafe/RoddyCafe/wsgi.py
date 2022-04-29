35d0c0838fbf49488d71606154be5ba07a4ad204
root <root@syner.top>
2022-04-30 07:07:07 +0800

    [chore]Server configurations

--- RoddyCafe/RoddyCafe/wsgi.py
+++ RoddyCafe/RoddyCafe/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RoddyCafe.settings')
-
+sys.path.append("/home/www/RoddyCafe/RoddyCafe")
 application = get_wsgi_application()
