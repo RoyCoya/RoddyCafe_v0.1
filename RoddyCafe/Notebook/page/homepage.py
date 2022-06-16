529a3e943d451c9210b7428560ff52dc4a744eb5
RoyCoya <larysing@outlook.com>
2022-06-16 21:59:54 +0800

    [feat] Notebook homepage display unfinished notes

--- RoddyCafe/Notebook/page/homepage.py
+++ RoddyCafe/Notebook/page/homepage.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.shortcuts import render,redirect
+from django.db.models import Q
 
 from Notebook.models import *
 from CafeFrame.api.common import is_login
@@ -7,22 +8,47 @@ from CafeFrame.api.common import is_login
 # 主页
 def homepage(request):
     if not is_login(request): return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
-    unfinished_notes = note.objects.filter(isPending=True,directory__user=request.user)
+    unfinished_notes = note.objects.filter(isPending=True,directory__user=request.user).order_by('-editDate')
     dirs = directory.objects.filter(user=request.user)
     root_dir = dirs[0]
 
     # 按目录对笔记分组
-    unfinished_notes_groupby_dirs = unfinished_notes.values('directory').distinct()
-    for dir in unfinished_notes_groupby_dirs:
-        dir['directory'] = directory.objects.get(id=dir['directory'])
-        dir['notes'] = []
+    dirs = unfinished_notes.values('directory').distinct()
+    dir_ids = []
+    unfinished_notes_groupby_dir = []
+    for dir in dirs:
+        dir_ids.append(dir['directory'])
+    dir_ids = list(set(dir_ids))
+    for dir_id in dir_ids:
+        group = {}
+        dir = directory.objects.get(id=dir_id)
+        group['directory'] = dir
+        parents_directories = []
+        directory_parent = None
+        try: directory_parent = directory.objects.get(Q(first_child=dir)|Q(next_brother=dir))
+        except Exception as e: print(e)
+        dir_temp = dir
+        while(directory_parent):
+            if dir_temp == directory_parent.first_child:
+                parents_directories.append(directory_parent)
+            dir_temp = directory_parent
+            try:
+                directory_parent = directory.objects.get(Q(first_child=dir_temp)|Q(next_brother=dir_temp))
+            except Exception as e: directory_parent=None
+        parents_directories.reverse()
+        
+        group['parents'] = parents_directories
+        group['notes'] = []
+        unfinished_notes_groupby_dir.append(group)
+
     for unfinished_note in unfinished_notes:
-        for dir in unfinished_notes_groupby_dirs:
+        for dir in unfinished_notes_groupby_dir:
             if dir['directory'] == unfinished_note.directory:
                 dir['notes'].append(unfinished_note)
-    print(unfinished_notes_groupby_dirs)
+
+    print(unfinished_notes_groupby_dir)
     context = {
-        'unfinished_notes_groupby_dirs' : unfinished_notes_groupby_dirs,
+        'unfinished_notes_groupby_dir' : unfinished_notes_groupby_dir,
         'root_dir' : root_dir,
     }
     
